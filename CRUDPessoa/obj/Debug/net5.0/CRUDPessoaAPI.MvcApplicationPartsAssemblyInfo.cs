129605f34630668f0374531f2446d4e596cec0ef
Gustavo Rodrigues <gustavo.rodrigues@prks.com.br>
2021-06-16 10:21:06 -0300

    Removendo pasta

--- CRUDPessoa/obj/Debug/net5.0/CRUDPessoaAPI.MvcApplicationPartsAssemblyInfo.cs
+++ CRUDPessoa/obj/Debug/net5.0/CRUDPessoaAPI.MvcApplicationPartsAssemblyInfo.cs
@@ -11,13 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Aplicacao")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Aplicacao")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Aplicacao")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
 
